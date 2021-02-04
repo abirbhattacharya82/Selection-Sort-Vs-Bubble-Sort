2d69498acde22e114aff72df5674650adeda4a20
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2021-02-04 17:09:59 +0530

    Add files via upload

---
+++ Selection Sort/Selection.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+void selection_sort(int a[], int n)
+{
+	int i,j,temp,min,pos;
+	for(i=0;i<n-1;i++)
+	{
+		min=a[i];
+		pos=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(min>a[j])
+			{
+				min=a[j];
+				pos=j;
+			}
+		}
+		temp=min;
+		a[pos]=a[i];
+		a[i]=min;
+	}
+}
+void main()
+{
+	int i;
+	int a[]={9,7,10,2,1};
+	selection_sort(a,5);
+	for(i=0;i<5;i++)
+		printf("%d ",a[i]);
+}
